38e1290b01968d0549c4e6644ef4239b0d9259a7
Jonas Schell <jonasschell@me.com>
2015-04-28 18:12:28 -0400

    Added back and pause button to game scene. Pause at the moment only works for the fish and spear.

---
+++ Assets/Scripts/StartScreenScripts/EventSystemScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventSystemScript : MonoBehaviour {
+
+	public int gameLevelToLoad;
+	private bool gamePaused = false;
+
+	public void LoadGameLevel()
+	{
+		StartCoroutine (LoadLevel());
+	}
+
+	IEnumerator LoadLevel()
+	{
+		//unpause the game so the fade can take place
+		if (gamePaused)
+			gamePaused = false;
+
+		//Start fading out sequence bevor loading new level and wait until it is finished
+		float fadeTime = GameObject.Find ("Main Camera").GetComponent<SceneFading> ().BeginFade (1);
+		Debug.Log ("FadingTime: " + fadeTime);
+		yield return new WaitForSeconds(fadeTime);
+
+		//Load StoryWeel level
+		Application.LoadLevel (1);
+	}
+
+	public void PauseGame()
+	{
+		gamePaused = !gamePaused;
+		Debug.Log ("Pause" + gamePaused);
+
+		if (gamePaused) {
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+		}
+	}
+}
