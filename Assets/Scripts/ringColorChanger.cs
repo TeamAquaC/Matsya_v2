6fbb48287d0f0e7ae5c8a756938d243ef031e25b
artteam <whalehousestudios@gmail.com>
2015-05-04 15:07:20 -0400

    attempt at color change

--- Assets/Scripts/ringColorChanger.cs
+++ Assets/Scripts/ringColorChanger.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 
 public class ringColorChanger : MonoBehaviour {
-
+	SpriteRenderer sprRender;
+	Color color;
+	float scaleX;
 	// Use this for initialization
 	void Start () {
-
-	
+		sprRender = gameObject.GetComponent<SpriteRenderer>();
+		color = sprRender.color;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		scaleX = gameObject.transform.parent.localScale.x/2;
+		Debug.Log("scaleValue = " + scaleX);
+		sprRender.color = new Color((255 / scaleX), (255 / scaleX), 255);
+		
 	}
 }
