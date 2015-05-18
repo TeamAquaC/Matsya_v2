1f961e7e148798d09a4b0eb2b404bde93e198c0e
Jonas Schell <jonasschell@me.com>
2015-05-18 13:27:14 -0400

    Added level selector to story wheel

---
+++ Assets/Scripts/StartScreenScripts/LevelSelecter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelecter : MonoBehaviour {
+
+	int levelSelected;
+
+	void Update()
+	{
+
+		float rotZ = gameObject.transform.rotation.eulerAngles.z ;
+		levelSelected = Mathf.RoundToInt(((rotZ)/36f)-0.5f);
+		Debug.Log ("You selected Level__: " + levelSelected);
+
+	}
+	
+
+
+}
