26e7eb46b7945dd375a92f1fb98eec6fc0a9e89c
jbrnsk <jbrnsk@gmail.com>
2015-05-22 17:57:08 -0400

    Segments Cover Story Wheel, Check if Unlocked
    The Segments hiding the story wheel now check if the appropriate level
    is unlocked, and disappear when it is.

---
+++ Assets/Scripts/CheckIfUnlocked.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CheckIfUnlocked : MonoBehaviour {
+
+	int levelCheck;
+
+	// Use this for initialization
+	void Start () {
+
+		levelCheck = int.Parse (gameObject.name);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (MasterGameManager.Levels[levelCheck - 1].unlocked){
+			gameObject.GetComponent<SpriteRenderer>().enabled = false;
+		}
+
+	}
+}
